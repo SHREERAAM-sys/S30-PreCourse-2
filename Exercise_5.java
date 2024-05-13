d2ba0bde69188cb02f87ed33cb52732c075c3a3d
shreeraam-sys <shreeraamr@gmail.com>
2024-05-12 22:04:57 -0400

    Code implementation for iterative quick sort with stack

--- Exercise_5.java
+++ Exercise_5.java
@@ -1,7 +1,11 @@
-class IterativeQuickSort { 
+import java.util.Stack;
+class IterativeQuickSort {
     void swap(int arr[], int i, int j) 
     { 
-	//Try swapping without extra variable 
+	//Try swapping without extra variable
+        int temp =arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     } 
   
     /* This function is same in both iterative and 
@@ -9,12 +13,79 @@ class IterativeQuickSort {
     int partition(int arr[], int l, int h) 
     { 
         //Compare elements and swap.
+
+        int pivot = arr[l];
+        int i=l, j=h;
+
+        while(i<j){
+
+            //find a higher element in left greater thena pivot;
+            while(arr[i] <= pivot && i<h){
+                i++;
+            }
+
+            //fing element lesser than privot in the right side of the pivot
+            while(arr[j] > pivot && j>0) {
+                j--;
+            }
+
+            if(i<j) swap(arr, i, j);
+        }
+
+        swap(arr, l, j);
+
+        return j;
+
     } 
   
     // Sorts arr[l..h] using iterative QuickSort 
     void QuickSort(int arr[], int l, int h) 
     { 
         //Try using Stack Data Structure to remove recursion.
+
+        Stack<Integer> stack = new Stack();
+
+        stack.push(l);
+        stack.push(h);
+
+
+        // if(l<h){
+
+        //     int partition = partition(arr, l, h);
+
+        //     QuickSort(arr, l, partition-1);
+        //     QuickSort(arr, partition+1, h);
+        // }
+
+
+        while(!stack.isEmpty() && stack.size() >= 2){
+
+            int high = stack.pop();
+            int low  = stack.pop();
+
+            if(low<high) {
+
+                int partition = partition(arr, low, high);
+
+                //sorting space indexs for the left side of the partition
+                int leftLow = low, leftHigh = partition-1;
+
+                //sorting space indexs for the right side of the partition
+                int rightLow = partition+1, rightHigh = high;
+
+                if(leftLow < leftHigh) {
+                    stack.push(leftLow);
+                    stack.push(leftHigh);
+                }
+
+                if(rightLow < rightHigh) {
+                    stack.push(rightLow);
+                    stack.push(rightHigh);
+                }
+
+            }
+
+        }
     } 
   
     // A utility function to print contents of arr 
